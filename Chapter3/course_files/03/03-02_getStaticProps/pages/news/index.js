96d4b7cad924f0a15c17086431777dff826650c2
SliperyTracs <shoujun032@gmail.com>
2022-11-09 08:51:43 +0800

    chapter3

---
+++ Chapter3/course_files/03/03-02_getStaticProps/pages/news/index.js
@@ -0,0 +1,18 @@
+import Layout from "../../components/layout"
+
+export default function News() {
+    return(
+    <Layout>
+    <h1>Top Stories</h1>
+    </Layout>
+    )
+}
+
+const API_KEY = "9hUvOqGGdnCBvGKg4EB3L7mGdBC8hKKJ"
+export async function getStaticProps() {
+  // The value of the `props` key will be
+  //  passed to the `Home` component
+  return {
+    props: {}
+  }
+}
